ce417d4637e8ecd111a7712661be7b33e9875a4c
JoseMzero <58148716+JoseMzero@users.noreply.github.com>
2020-12-08 23:42:45 +0000

    sda

--- arbito.c
+++ arbito.c
@@ -33,7 +33,48 @@ int main(int argc, char** argv) {
 		exit(1);
 	}
 	
+<<<<<<< Updated upstream
 	printf("asdasdad3");
+=======
+	
+	int kickPlayer(cli *Users,char cmd[],int max){
+		int i;	
+
+		for(i=0;i<max;i++)
+  			if((Users+i)->aceite==1)
+  				if(strcmp(cmd, (Users+i)->nome)==0){
+  					(Users+i)->aceite = -1;
+  					
+  					kill((Users+i)->pid, SIGUSR1);
+  					return 0;
+  				}
+  		return -1; //erro
+	}
+	
+	/*########################################## USER COMANDOS ##########################################*/
+	
+	int interpretaCmdUser(char cmd[]){
+		if(cmd[0]=='#'){
+			if(strcmp(cmd, "#mygame")==0){
+			
+			}
+			else if(strcmp(cmd, "#quit")==0){
+			
+			}
+		}
+		return 0;
+	}
+	
+	
+	
+	
+	
+
+	
+	
+
+int main(int argc, char** argv) {
+>>>>>>> Stashed changes
 
 	if (argc < 3) {
 			printf("ERRO - FALTAMARGUMENTOS NA LINHA DE COMANDOS\n");
@@ -53,7 +94,48 @@ int main(int argc, char** argv) {
 	
 	
 
+<<<<<<< Updated upstream
 	printf("asdasdad4");
+=======
+				}
+				else if(strcmp(cmd, "games")==0){
+					//lista jogos
+				}
+				else if(cmd[0]=='k'){
+					memmove(cmd, cmd+1, strlen(cmd));
+					kickPlayer(clientes, cmd, max_player);
+				}
+		}
+		else if(res>0 && FD_ISSET(fd, &fds)){
+			
+			//comunicaçoes
+				
+				num = read(fd, &c, sizeof(cli));
+		
+				if(num == sizeof(cli)){
+					if(c.aceite == -1){
+						addUser(&c, clientes, &nPlayers, max_player);
+					}
+						
+					interpretaCmdUser(c.res);
+						
+					sprintf(fifo, FIFO_CLI, c.pid); //poder saber a quem responder
+					fdr = open(fifo, O_WRONLY);
+			
+					num = write(fdr, &c, sizeof(cli));
+					close(fdr);
+				}
+		}		
+		
+	}while(termina != true);
+	
+	close(fd);
+	unlink(FIFO_SRV);
+	
+	exit(5);
+	
+	
+>>>>>>> Stashed changes
 
 	cli m;
 	char nome[50];
--- cliente.c
+++ cliente.c
@@ -1,6 +1,17 @@
 #include "utils.h"
 
+void terminarExecucao();
+
 int main(int argc, char** argv){
+<<<<<<< Updated upstream
+=======
+	char fifo[40];
+	int fd_out, fd_in, num;
+	cli j;
+	
+	signal(SIGUSR1, terminarExecucao);
+
+>>>>>>> Stashed changes
 	
 	if (access(PIPE_SERVER, F_OK) != 0){
 		printf("ERRO - NAO FOI POSSIVEL ENCONTRAR O ARBITO");
@@ -25,4 +36,13 @@ int main(int argc, char** argv){
 	
 	
 
+}
+
+void terminarExecucao(){
+	printf("\nKickado do Servidor...\n");
+	char temp[20];
+	sprintf(temp, FIFO_CLI, getpid());
+	unlink(temp);
+	
+	exit(0);
 }
